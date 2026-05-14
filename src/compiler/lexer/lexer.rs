2a7eca74dc79dfffc1d064d05c1f0579063a026d
= <noahjhan11@gmail.com>
2026-05-14 01:56:49 -0500

    comment lexer

--- src/compiler/lexer/lexer.rs
+++ src/compiler/lexer/lexer.rs
@@ -16,6 +16,8 @@ impl Lexer {
         Lexer { source }
     }
 
+    // public interface for processing tokens
+    // wrapper for advance_token() with error recovery
     pub fn next(&mut self) -> Result<Option<Token>, RatError> {
         match self.advance_token() {
             Err(err @ RatError::LexicalError(_)) => {
@@ -26,6 +28,7 @@ impl Lexer {
         }
     }
 
+    // returns and consumes next token in source file
     fn advance_token(&mut self) -> Result<Option<Token>, RatError> {
         match self.peek()? {
             None => return Ok(None),
@@ -66,6 +69,7 @@ impl Lexer {
         }
     }
 
+    // fills buffer with the current escape sequence
     fn advance_escape_sequence(
         &mut self,
         buf: &mut String,
@@ -101,6 +105,7 @@ impl Lexer {
         Ok(())
     }
 
+    // single-line string literals
     fn advance_string_literal(&mut self) -> Result<Option<Token>, RatError> {
         let start_pos = self.source.position();
         let mut buf = self.expect_opening_char('"', start_pos)?;
@@ -130,27 +135,49 @@ impl Lexer {
         let start_pos = self.source.position();
         let mut buf = self.expect_opening_char('\'', start_pos)?;
 
-        loop {
-            let ch = match self.read()? {
-                Some(ch) => ch,
-                None => {
-                    return Err(RatError::lexical(
-                        "missing \"'\" at the end of character literal",
-                        &buf,
-                        Span::set(start_pos, self.source.position()),
-                    ))
-                }
-            };
-
-            buf.push(ch);
-            match ch {
-                '\\' => self.advance_escape_sequence(&mut buf, start_pos)?,
-                '\'' => return self.emit(Category::Literal, buf, start_pos),
-                _ => {}
+        let ch = match self.read()? {
+            Some(ch) => ch,
+            None => {
+                return Err(RatError::lexical(
+                    "unterminated character literal",
+                    &buf,
+                    Span::set(start_pos, self.source.position()),
+                ))
             }
+        };
+
+        match ch {
+            '\\' => {
+                self.advance_escape_sequence(&mut buf, start_pos)?;
+            }
+            '\'' => {
+                return Err(RatError::lexical(
+                    "empty character literal",
+                    &buf,
+                    Span::set(start_pos, self.source.position()),
+                ));
+            }
+            _ => {
+                buf.push(ch);
+            }
+        }
+
+        match self.read()? {
+            Some('\'') => self.emit(Category::Literal, buf, start_pos),
+            Some(_) => Err(RatError::lexical(
+                "character literal must contain exactly one character",
+                &buf,
+                Span::set(start_pos, self.source.position()),
+            )),
+            None => Err(RatError::lexical(
+                "unterminated character literal",
+                &buf,
+                Span::set(start_pos, self.source.position()),
+            )),
         }
     }
 
+    // internal error upon malformed function call
     fn expect_opening_char(
         &mut self,
         expected: char,
@@ -171,6 +198,7 @@ impl Lexer {
         }
     }
 
+    // rust style unicode escape sequence
     fn advance_unicode_escape(
         &mut self,
         partial: &str,
@@ -237,6 +265,7 @@ impl Lexer {
         Ok(format!("\\u{{{}}}", hex))
     }
 
+    // fill buffer until peek() returns a non ascii digit
     fn read_numerics(&mut self, buf: &mut String) -> Result<bool, RatError> {
         let mut read_any = false;
         while let Some(ch) = self.peek()? {
@@ -320,6 +349,7 @@ impl Lexer {
         }
     }
 
+    // when at the end of a token, require next char to be a delimiter
     fn expect_delimiter_then_emit(
         &mut self,
         category: Category,
@@ -336,6 +366,7 @@ impl Lexer {
         }
     }
 
+    // error recovery, try to read ahead as far as possible
     fn read_until_delimiter(&mut self) -> Result<(), RatError> {
         loop {
             match self.peek()? {
@@ -348,6 +379,8 @@ impl Lexer {
         }
     }
 
+    // skip non-newline whitespace
+    // newline is a separate punctuator
     fn advance_whitespace(&mut self) -> Result<bool, RatError> {
         loop {
             match self.peek()? {
@@ -360,6 +393,8 @@ impl Lexer {
         }
     }
 
+    // check if the partial + peek() is a type, keyword, operator, or punctuator
+    // if so, current partial can be extended
     fn should_advance(&mut self, partial: &str) -> Result<bool, RatError> {
         let Some(ch) = self.peek()? else {
             return Ok(false);
@@ -369,14 +404,17 @@ impl Lexer {
         Ok(Category::is_any(&extended).is_some())
     }
 
+    // source.read() wrapper
     fn read(&mut self) -> Result<Option<char>, RatError> {
         Ok(self.source.read()?.map(|b| b as char))
     }
 
+    // source.peek() wrapper
     fn peek(&mut self) -> Result<Option<char>, RatError> {
         Ok(self.source.peek()?.map(|b| b as char))
     }
 
+    // Token::new() wrapper
     fn emit(
         &mut self,
         category: Category,
--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,6 @@ fn main() {
     println!("{}", ascii_ratty);
     println!("bonjour le monde!");
 
-    let filepath = "data/numeric_literal.txt";
+    let filepath = "data/compile.rat";
     compile(filepath, true);
 }
